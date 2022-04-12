23beea16a198a17cc35082108db348a585c89272
abihedia <51483423+abihedia@users.noreply.github.com>
2022-04-12 09:14:07 +0100

    Update models.py

--- fleetADD1/models/models.py
+++ fleetADD1/models/models.py
@@ -19,21 +19,28 @@ class FleetContINHERIT(models.Model):
     def solde_estimatif(self):
         self.fleet_solde_est = self.fleet_prix_HT*self.fleet_duree_rest
 
-    @api.depends('fleet_expiration_date')
-    @api.onchange('fleet_date_inst','fleet_duree','fleet_periodicite','fleet_expiration_date')
+    @api.depends('fleet_date_inst')
+    @api.onchange('fleet_date_inst', 'fleet_duree', 'fleet_periodicite', 'fleet_expiration_date')
     def duree_rest(self):
         if self.fleet_expiration_date:
-            if self.fleet_expiration_date > date.today():
+            if self.fleet_expiration_date >= date.today():
+                nombre_jour = date.today() - self.fleet_date_inst
                 if self.fleet_periodicite == 'mens':
-                    self.fleet_duree_rest = (self.fleet_expiration_date - date.today()).days / 30
+                    self.fleet_duree_rest = nombre_jour.days / 30
                 elif self.fleet_periodicite == 'trim':
-                    self.fleet_duree_rest = (self.fleet_expiration_date - date.today()).days / 90
+                    self.fleet_duree_rest = nombre_jour.days / 90
                 else:
                     self.fleet_duree_rest = 0
             else:
+                if self.fleet_periodicite == 'mens':
+                    self.fleet_duree_rest = (self.fleet_expiration_date - self.date.today()).days / 30
+                elif self.fleet_periodicite == 'trim':
+                    self.fleet_duree_rest = (self.fleet_expiration_date - self.date.today()).days / 90
+                else:
                     self.fleet_duree_rest = 0
         else:
             self.fleet_duree_rest = 0
+            
 
 
 
