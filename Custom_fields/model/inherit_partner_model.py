9fe838de15822e86b5aee560835589773d427349
abihedia <51483423+abihedia@users.noreply.github.com>
2022-04-20 10:39:51 +0100

    Delete inherit_partner_model.py

--- Custom_fields/model/inherit_partner_model.py
+++
@@ -1,9 +0,0 @@
-from odoo import models, fields, api
-
-class PartnerHerit(models.Model):
-    _inherit = 'res.partner'
-
-    parc_machine = fields.One2many('fleet.vehicle', 'partner_id')
-    num_siren = fields.Char('N° sirene')
-    activity = fields.Char('activité')
-    x_type_contact = fields.Selection([('societe', 'societe'), ('particulier', 'particulier')])
