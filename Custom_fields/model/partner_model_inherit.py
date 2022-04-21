44d81e779cc6443db15a9ddc6e7a53fc49c22729
abihedia <51483423+abihedia@users.noreply.github.com>
2022-04-21 09:38:52 +0200

    Add files via upload

---
+++ Custom_fields/model/partner_model_inherit.py
@@ -0,0 +1,9 @@
+from odoo import models, fields, api
+
+class PartnerModelHerit(models.Model):
+    _inherit = 'res.partner'
+
+    type_contact = fields.Selection([('Prospect', 'Prospect'), ('Client', 'Client')])
+    parc_machine = fields.One2many('fleet.vehicle', 'partner_id')
+    num_siren = fields.Char('N° sirene')
+    activity = fields.Char('activité')
